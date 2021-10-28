9c45e248ae2ec16ece4defed4e57ef72efa0e027
Danielevron <daniel.evron@post.idc.ac.il>
2021-10-28 16:27:20 +0300

    h

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,10 +4,9 @@ public class Main {
 
     public static void main(String[] args) {
 	    System.out.println("hello evreybody");
-        
-
-
-
+for(int i=10;i>1;i--){
+    System.out.println("the value of i is:"+i);
+        }
 
 
 
